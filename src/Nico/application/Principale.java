bf23238de583a40c4041511f042866d42dbf7621
nicolas duval <nicolas.duval@maformationenligne.bzh>
2018-08-28 17:12:06 +0200

    a finaliser

--- src/Nico/application/Principale.java
+++ src/Nico/application/Principale.java
@@ -4,7 +4,25 @@ import Nico.entites.Personne;
 public class Principale {
 
     public static void main(String[] args){
-        
+        System.out.println("Passage dans main");
+
+
+            Personne Martin = new Personne ("Martin", "JAVA SARL" );
+            Personne Dupont = new Personne ("Dupont");
+
+            Martin.afficher();
+            Dupont.afficher();
+
+            Personne durand = new Personne("durand", "J2E SA");
+
+            durand.afficher();
+            durand.quitterSociete();
+            durand.afficher();
+
+            durand.setSociete("EJB COrporate");
+            durand.afficher();
+            durand.setSociete("java SA");
+            durand.afficher();
     }
 
 }
--- src/Nico/entites/Personne.java
+++ src/Nico/entites/Personne.java
@@ -12,11 +12,13 @@ public class Personne {
 
     public Personne(String nom, String societe) {
         this.nom = nom.toUpperCase();
-        this.societe = societe;
+        validerSociete(societe.toUpperCase());
     }
+
 
     public Personne(String nom){
         this.nom = nom.toUpperCase();
+        this.societe = "?";
     }
 //----------------------Getters---------------------//
 
@@ -30,8 +32,15 @@ public class Personne {
 //----------------------Setters--------------------//
 
     public void setSociete(String societe) {
-        this.societe = societe;
+        if (PAS_DE_SOCIETE.equals(this.societe)){
+            validerSociete(societe);
+        }
+        else {
+
+            System.out.println("bye bye");
+            System.exit(2);}
     }
+
 
 //----------------------Methodes-------------------------//
 
@@ -43,4 +52,30 @@ public class Personne {
         else {System.out.println(getNom() + " travaille chez " +getSociete());}
     }
 
+    public void quitterSociete(){
+        if (societe.equals(PAS_DE_SOCIETE)){
+            System.out.println("Je ne suis pas salarié donc je fais bugger ton appli!!!!!!");
+            System.exit(1);
+        }
+        else {this.societe=PAS_DE_SOCIETE;}
+    }
+
+    private void validerSociete(String societe){
+        if ((societe.length()<= 30)) {
+            this.societe = societe.toUpperCase();
+        }
+     /*   else if(!societe.equals(PAS_DE_SOCIETE)){
+            System.out.println("on ne peut pas integrer une seconde entreprise!!!! RESULTAT ---> BUG");
+            System.out.println("il faut demissionner avant!!!!");
+            System.exit(1);
+        }*/
+        else {
+            System.out.println("le nom de la societe doit faire moins de 30 caracteres et different de '?'!!!! RESULTAT ---> BUG");
+            System.out.println("le nom de societe posant probleme est : " + societe);
+            System.exit(3);
+        }
+    }
 }
+
+
+
